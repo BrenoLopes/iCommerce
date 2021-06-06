c5ddbd18bd133fd42f42f82dae68c884d37e588d
brenolopes <brenolprimo@gmail.com>
2021-06-06 09:48:59 -0400

    Moved the app into the app folder and installed node-sass

---
+++ frontend/src/app/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+
+import Button from './components/button/Button';
+
+function App() {
+  return (
+    <div>
+      <Button />
+    </div>
+  );
+}
+
+export default App;
