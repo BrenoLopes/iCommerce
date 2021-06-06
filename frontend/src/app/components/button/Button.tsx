e90e56a44d1d0669129eba9e2dbea9b27261d5c5
brenolopes <brenolprimo@gmail.com>
2021-06-06 19:45:05 -0400

    Expanded the button component

--- frontend/src/app/components/button/Button.tsx
+++ frontend/src/app/components/button/Button.tsx
@@ -1,15 +1,40 @@
 import React, {FunctionComponent} from "react";
 import "./Button.scss";
 
+type Color = "primary" | "secondary" | "default"
+
 interface Props {
   onClick: () => void;
-  isFull: boolean | undefined;
+  isFull: boolean;
+  label: string;
+  color: Color
 }
 
-const Button: FunctionComponent<Props> = ({ onClick, isFull }: Props): JSX.Element => {
+const Button: FunctionComponent<Props> = (props: Props): JSX.Element => {
+
+  const buttonFull = props.isFull ? "btn-full" : ""
+  const colorCss = loadCssColor(props.color)
+
   return (
-    <button onClick={onClick} className={`button ${isFull ? "button-full" : ""}`}>Login</button>
+    <button
+      type="button"
+      onClick={props.onClick}
+      className={`btn ${buttonFull} ${colorCss}`}
+    >
+      {props.label}
+    </button>
   );
+}
+
+const loadCssColor = (color: Color) => {
+  switch (color) {
+    case "primary":
+      return "btn-primary"
+    case "secondary":
+      return "btn-secondary"
+    default:
+      return ""
+  }
 }
 
 export default Button
