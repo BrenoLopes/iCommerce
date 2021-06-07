9cb25f5b808765e7c325b657a15fbe3fcbabd0bb
brenolopes <brenolprimo@gmail.com>
2021-06-07 18:38:46 -0400

    Created a basic login page

--- frontend/src/app/pages/login/LoginPage.tsx
+++ frontend/src/app/pages/login/LoginPage.tsx
@@ -1,75 +1,86 @@
-import react, { useEffect, useState } from 'react'
-import { useHistory } from 'react-router-dom'
-import logo from 'iCommerce.svg'
+import react, { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+import logo from "iCommerce.svg";
 
-import { validateCharLength, validateEmptyInput } from "../../components/validators";
+import {
+  validateCharLength,
+  validateEmptyInput,
+} from "../../components/validators";
 import FormInputComponent from "app/components/forminput/FormInputComponent";
 import CheckboxInput from "app/components/checkbox/CheckboxComponent";
 import ButtonComponent from "app/components/button/ButtonComponent";
 import SeparatorComponent from "app/components/separator/SeparatorComponent";
 
-import "./LoginPage.scss"
+import "./LoginPage.scss";
 import Network from "../../networking/url";
-import {LoginRequest, LoginResponse} from "./types";
+import { LoginRequest, LoginResponse } from "./types";
 
-const LoginPage : react.FC = () => {
+const LoginPage: react.FC = () => {
   const [username, setUsername] = useState({
     user: "",
-    isValid: false
-  })
+    isValid: false,
+  });
   const [userPasswordData, setUserPasswordData] = useState({
     user: "",
-    isValid: false
-  })
-  const [rememberPassword, setRememberPassword] = useState(true)
-  const [isFormDisabled, setIsFormDisabled] = useState(true)
+    isValid: false,
+  });
+  const [rememberPassword, setRememberPassword] = useState(true);
+  const [isFormDisabled, setIsFormDisabled] = useState(true);
+
+  const history = useHistory();
 
   useEffect(() => {
-    if (username.isValid && userPasswordData.isValid)
-      setIsFormDisabled(false)
-    else
-      setIsFormDisabled(true)
-  }, [username, userPasswordData])
+    if (localStorage.getItem('token') === null) return
 
-  const history = useHistory()
+    history.push("/produtos")
+  }, [])
+
+  useEffect(() => {
+    if (username.isValid && userPasswordData.isValid) setIsFormDisabled(false);
+    else setIsFormDisabled(true);
+  }, [username, userPasswordData]);
 
   const onLogin = async () => {
     const loginData: LoginRequest = {
       username: username.user,
-      password: userPasswordData.user
-    }
+      password: userPasswordData.user,
+    };
 
     try {
-      const result = await new Network<LoginResponse>(false).post("/auth/login", loginData, {
-        headers: {'Content-Type': 'application/json'}
-      })
+      const result = await new Network<LoginResponse>(false).post(
+        "/auth/login",
+        loginData,
+        {
+          headers: { "Content-Type": "application/json" },
+        }
+      );
 
-      localStorage.setItem('token', result.data.access_token)
+      localStorage.setItem("token", result.data.access_token);
 
-      history.push("/produtos")
-    } catch (e) {
-
-    }
-  }
+      history.push("/produtos");
+    } catch (e) {}
+  };
 
   const onSignUp = () => {
-    history.push("/cadastrar")
-  }
+    history.push("/cadastrar");
+  };
 
   return (
     <div className="login-page-container">
       <div className="page-top" />
       <div className="form-container">
         <div className="website-logo">
-          <img src={logo} alt=""/>
+          <img src={logo} alt="" />
         </div>
         <FormInputComponent
           label="Usuário"
           validators={[validateEmptyInput(), validateCharLength(50)]}
-          onChange={(text, isValid) => setUsername({
-            user: text,
-            isValid
-          })}
+          onChange={(text, isValid) =>
+            setUsername({
+              user: text,
+              isValid,
+            })
+          }
           type="text"
         />
         <FormInputComponent
@@ -79,25 +90,39 @@ const LoginPage : react.FC = () => {
             validateCharLength(50),
             // { message: "A senha não pode ser menor que 6 caracteres", test: (text) => text.length < 7 }
           ]}
-          onChange={(text, isValid) => setUserPasswordData({
-            user: text,
-            isValid
-          })}
+          onChange={(text, isValid) =>
+            setUserPasswordData({
+              user: text,
+              isValid,
+            })
+          }
           type="password"
         />
         <CheckboxInput
           default={rememberPassword}
           onChange={(state) => {
-            setRememberPassword(state)
+            setRememberPassword(state);
           }}
           label={"Lembrar senha"}
         />
-        <ButtonComponent label="Login" isFull={true} color="primary" onClick={onLogin} isDisabled={isFormDisabled} />
+        <ButtonComponent
+          label="Login"
+          isFull={true}
+          color="primary"
+          onClick={onLogin}
+          isDisabled={isFormDisabled}
+        />
         <SeparatorComponent label="ou" color="dark" />
-        <ButtonComponent label="Cadastrar" isFull={true} color="default" onClick={onSignUp} isDisabled={false} />
+        <ButtonComponent
+          label="Cadastrar"
+          isFull={true}
+          color="default"
+          onClick={onSignUp}
+          isDisabled={false}
+        />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default LoginPage
+export default LoginPage;
