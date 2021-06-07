a369801dcd47013c5ae00aa4911eead961d162d1
brenolopes <brenolprimo@gmail.com>
2021-06-07 19:51:47 -0400

    Removed code that doesnt work

--- backend/src/main/kotlin/br/balladesh/icommerce/security/auth/TokenHandler.kt
+++ backend/src/main/kotlin/br/balladesh/icommerce/security/auth/TokenHandler.kt
@@ -39,19 +39,6 @@ class TokenAuthenticationFilter(
   }
 }
 
-@Component
-class CorsFilter : Filter {
-  @Throws(IOException::class, ServletException::class)
-  override fun doFilter(req: ServletRequest?, res: ServletResponse, chain: FilterChain) {
-    val response = res as HttpServletResponse
-    response.setHeader("Access-Control-Allow-Origin", "*")
-    response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE")
-    response.setHeader("Access-Control-Max-Age", "3600")
-    response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With")
-    chain.doFilter(req, res)
-  }
-}
-
 data class TokenBasedAuthentication(
   private var token: String,
   private val userDetails: UserDetails
