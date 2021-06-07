a96859b93cd42f8c25bd96544c05b6efa36a6133
brenolopes <brenolprimo@gmail.com>
2021-06-07 12:32:18 -0400

    Setup the component's path in the app component

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import LoginPage from "app/pages/login/LoginPage";
+import SignupPage from "app/pages/signup/SignupPage";
 
 import './App.scss';
 
 function App() {
   return (
-    <div>
+    <div className="app-root">
+      <Switch>
+        <Route path="/" component={LoginPage} exact />
+        <Route path="/cadastrar" component={SignupPage} />
+      </Switch>
     </div>
   )
 }
