f1437877e3e2563622394c9bcfe3781b9a7ab6c6
brenolopes <brenolprimo@gmail.com>
2021-06-07 22:06:59 -0400

    imnsonia file for testing and a build script

---
+++ build.sh
@@ -0,0 +1,16 @@
+#/bin/sh
+
+cd ./frontend
+if [ ! -d "./build" ]; then
+	rm -rf ./build
+fi
+npm run build
+if [ ! -d "../backend/src/main/resources/public" ]; then
+	mkdir ../backend/src/main/resources/public
+else
+	rm -rf ../backend/src/main/resources/public/*
+fi
+mv build/* ../backend/src/main/resources/public/
+cd ../backend
+mvn package -DskipTests
+mv ./target/iCommerce-0.0.1-SNAPSHOT.jar ../../app.jar
